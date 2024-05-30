feb967ef200abc563946087a378ae82ee2dc9636
Harmeet <hs978698@gmail.com>
2024-05-30 22:34:00 +0530

    linear sort with all indexes and frequency of the target element

--- Java/linearsearch.java
+++ Java/linearsearch.java
@@ -9,6 +9,13 @@ public class linearsearch {
         }
         System.out.println("Enter the target element");
         int target=in.nextInt();
+        //search number of frequency of target element at what position
+        System.out.println("The target element is present at positions ");
+        for (int i=0;i<n;i++){
+            if (arr[i]==target){
+                System.out.println(i+1);
+            }
+        }
         //search number of frequency of target element
         int count=0;
         for (int i=0;i<n;i++){
