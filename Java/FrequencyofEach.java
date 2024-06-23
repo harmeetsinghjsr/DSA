a7539d18593bc8e6ba9bcdd2a9930f92f9270945
Harmeet <hs978698@gmail.com>
2024-06-23 22:55:53 +0530

    with whitespace counted

--- Java/FrequencyofEach.java
+++ Java/FrequencyofEach.java
@@ -1,21 +1,21 @@
 public class FrequencyofEach {
     public static void main(String[] args) {
-        int c=0;
-        int len=0;
-        String s1="poppins are in my bag";
-        s1=s1.toUpperCase();
-        len=s1.length();
-        for(char j='A';j<='Z';j++)
-        {
-            c=0;
-            for(int i=0;i<len;i++)
-            {
-                char k=s1.charAt(i);
-                if(k==j)
-                c++;
+        int c = 0;
+        int len = 0;
+        String s1 = "poppins are in my bag";
+        s1 = s1.toUpperCase();
+        len = s1.length();
+        for (char j = 'A'; j <= 'Z'; j++) {
+            c = 0;
+            for (int i = 0; i < len; i++) {
+                char k = s1.charAt(i);
+                if (k == j || k == ' ') { // Include whitespace in the condition
+                    c++;
+                }
             }
-            if(c>0)
-            System.out.println("The frequency of "+j+" is "+c);
+            if (c > 0) {
+                System.out.println("The frequency of " + j + " is " + c);
+            }
         }
-    }    
+    }
 }
